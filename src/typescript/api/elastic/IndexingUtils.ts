9c4fbd5aef57f8b70b4f280c7ac9fbc5c71ef880
Alexander balashov <alexander85bal@gmail.com>
2024-03-22 22:03:26 +0200

    Move general utility functions into Indexing-utils.

---
+++ src/typescript/api/elastic/IndexingUtils.ts
@@ -0,0 +1,30 @@
+import { IDocumentBranch } from './branchServicesIndexing';
+
+export const bulkBranchDocuments = (bulkData: {
+	addBranches: IDocumentBranch[];
+	branchIndexName: string;
+}) => {
+	// Initialize an empty array to store the bulk data
+	let bulk: string[] = [];
+
+	// Iterate over each branch document in the addBranches array
+	bulkData.addBranches.forEach((branchDocument) => {
+		// Create the index metadata string for the current branch document
+		const indexMetadata = JSON.stringify({
+			index: {
+				_index: bulkData.branchIndexName,
+				_id: branchDocument.branchnumber.toString(),
+			},
+		});
+
+		// Convert the branch document to a JSON string
+		const stringedDocument = JSON.stringify(branchDocument);
+
+		// Push the index metadata and branch document strings to the bulk array
+		bulk.push(indexMetadata, stringedDocument);
+	});
+
+	// Join the bulk array elements with newline characters and add a trailing newline
+	const stringedBulk = bulk.join('\n') + '\n';
+	return stringedBulk;
+};
