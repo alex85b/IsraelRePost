1c10c5d8cacc6338ae1e982beffa762258c9bba4
Alexander balashov <alexander85bal@gmail.com>
2024-03-22 12:13:57 +0200

    Remove util functions from Elasticsearch Client into this

---
+++ src/typescript/api/elastic/base/elasticsearchUtils.ts
@@ -0,0 +1,36 @@
+import path from 'path';
+import fs from 'fs';
+
+import { ERR_RESOURCE_MISSING } from '../../../shared/constants/errorCodes';
+
+const MODULE_NAME = 'Elasticsearch Utils';
+
+type ElasticAuthentication = {
+	username: string;
+	password: string;
+	certificates: string;
+};
+
+type ElasticAuthenticationProvider = () => ElasticAuthentication;
+
+export const getAuthenticationData: ElasticAuthenticationProvider = () => {
+	const certificatePath = path.join(
+		__dirname,
+		'..',
+		'..',
+		'..',
+		'..',
+		'..',
+		'elastic-cert',
+		'http_ca.crt'
+	);
+	const certificates = fs.readFileSync(certificatePath, 'utf8');
+	const username = process.env.ELS_USR ?? '';
+	const password = process.env.ELS_PSS ?? '';
+	if (username === '' || password === '' || certificates === '') {
+		throw new Error(
+			`[${MODULE_NAME}][${ERR_RESOURCE_MISSING}] : No Username / password / certificates`
+		);
+	}
+	return { username, password, certificates };
+};
