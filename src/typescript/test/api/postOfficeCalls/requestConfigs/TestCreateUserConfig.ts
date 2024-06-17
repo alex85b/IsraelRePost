437d56c9cabf265b1e139451ab18134b0378a089
Alexander balashov <alexander85bal@gmail.com>
2024-06-17 21:20:36 +0300

    Added extensive testing for each major refactor or addition

--- src/typescript/test/api/postOfficeCalls/requestConfigs/TestCreateUserConfig.ts
+++ src/typescript/test/api/postOfficeCalls/requestConfigs/TestCreateUserConfig.ts
@@ -1,9 +1,11 @@
 import { postofficeApiCall } from '../../../../api/postOfficeCalls/base/PostofficeApiCall';
 import { buildUserCallConfig } from '../../../../api/postOfficeCalls/requestConfigs/CreateUserConfig';
+import { buildUsingProxyFile } from '../../../../data/models/dataTransferModels/ProxyEndpointString';
 import {
 	IExpectedUserResponse,
 	RequestUserResponse,
 } from '../../../../data/models/dataTransferModels/postofficeResponses/RequestUserResponse';
+import path from 'path';
 
 console.log('** Test Create-User Config **');
 
@@ -12,7 +14,38 @@ export const makeUserRequest = async () => {
 	const config = buildUserCallConfig();
 	// console.log('[makeUserRequest] request config', JSON.stringify(config, null, 3));
 	const response = await postofficeApiCall<IExpectedUserResponse>(config);
-	// console.log('[makeUserRequest] response : ', response);
+	const userResponse = new RequestUserResponse.Builder().useAxiosResponse(response).build();
+	// console.log('[makeUserRequest] Cookies : ', userResponse.getCookies());
+	// console.log('[makeUserRequest] Token : ', userResponse.getToken());
+	return userResponse;
+};
+
+export const makeUserRequestWithProxy = async () => {
+	console.log('** (2) Make User Request With Proxy **');
+
+	const envFilepath = path.join(__dirname, '..', '..', '..', '..', '..', '..', '.env');
+	console.log('[makeUserRequestWithProxy] path to env : ', envFilepath);
+	const proxyFilepath = path.join(
+		__dirname,
+		'..',
+		'..',
+		'..',
+		'..',
+		'..',
+		'..',
+		'SmartProxy.txt'
+	);
+	console.log('[makeUserRequestWithProxy] path to proxy file path : ', proxyFilepath);
+	const strings = await buildUsingProxyFile({
+		envFilepath,
+		proxyFilepath,
+		envPasswordKey: 'PROX_SMRT_PAS',
+		envUsernameKey: 'PROX_SMRT_USR',
+	});
+	console.log('[makeUserRequestWithProxy] strings demo : ', strings[0]);
+	const config = buildUserCallConfig(strings[0]);
+	// console.log('[makeUserRequest] request config', JSON.stringify(config, null, 3));
+	const response = await postofficeApiCall<IExpectedUserResponse>(config);
 	const userResponse = new RequestUserResponse.Builder().useAxiosResponse(response).build();
 	// console.log('[makeUserRequest] Cookies : ', userResponse.getCookies());
 	// console.log('[makeUserRequest] Token : ', userResponse.getToken());
